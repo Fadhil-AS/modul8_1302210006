ecbf7e4c78bf83b5dcab34629235de8588842030
Muhammad Fadhil Ardiansyah S <mfadhil661@gmail.com>
2023-04-12 17:30:41 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ modul8_1302210006/BankTransferConfig_1302210006.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_1302210006
+{
+    internal class BankTransferConfig_1302210006
+    {
+        public bankTransfer_1302210006 bankTf;
+        public const string bFile = @"bank_transfer_config.json";
+
+        public BankTransferConfig_1302210006() {
+            try
+            {
+                ReadConfigFile_1302210006();
+                Console.WriteLine("Membaca config file JSON");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Membuat config file default");
+                setDefault_1302210006();
+                WriteConfigFile_1302210006();
+            }
+        }
+
+        public void ReadConfigFile_1302210006() { 
+            string jsontxt = File.ReadAllText(bFile);
+            bankTf = JsonSerializer.Deserialize<bankTransfer_1302210006>(jsontxt);
+        }
+
+        public void WriteConfigFile_1302210006()
+        {
+            JsonSerializerOptions opt = new JsonSerializerOptions()
+            {
+                WriteIndented = true,
+            };
+            String jsonText = JsonSerializer.Serialize(bankTf, opt);
+            File.WriteAllText(bFile, jsonText);
+        }
+
+        public void setDefault_1302210006() { 
+            bankTf = new bankTransfer_1302210006();
+            bankTf.lang = "en";
+            bankTf.transfer.threshold = 25000000;
+            bankTf.transfer.low_fee = 6500;
+            bankTf.transfer.high_fee = 15000;
+            //List<string> methods = new List<string>();
+            bankTf.methods.Add("RTO (real-time)");
+            bankTf.methods.Add("SKN");
+            bankTf.methods.Add("RTGS");
+            bankTf.methods.Add("BI FAST");
+            bankTf.confirmation.en = "yes";
+            bankTf.confirmation.id = "ya";
+        }
+
+    }
+
+    public class bankTransfer_1302210006 {
+        public string lang { set; get; }
+        public Transfer_1302210006 transfer { set; get; }
+        public List<string> methods { set; get; }
+        public Confirmation_1302210006 confirmation { set; get; }
+        public bankTransfer_1302210006() { }
+        public bankTransfer_1302210006(string lang, Transfer_1302210006 transfer, List<string> methods, Confirmation_1302210006 confirmation)
+        {
+            this.lang = lang;
+            this.transfer = transfer;
+            this.methods = methods;
+            this.confirmation = confirmation;
+        }
+    }
+
+    public class Transfer_1302210006 {
+        public int threshold;
+        public int low_fee;
+        public int high_fee;
+
+        public Transfer_1302210006() { }
+        public Transfer_1302210006(int threshold, int low_fee, int high_fee)
+        {
+            this.threshold = threshold;
+            this.low_fee = low_fee;
+            this.high_fee = high_fee;
+        }
+    }
+
+    public class Confirmation_1302210006 { 
+        public string en { set; get; }
+        public string id { set; get; }
+
+        public Confirmation_1302210006() { }
+
+        public Confirmation_1302210006(string en, string id)
+        {
+            this.en = en;
+            this.id = id;
+        }
+    }
+}
--- modul8_1302210006/Program.cs
+++ modul8_1302210006/Program.cs
@@ -1,2 +1,77 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using modul8_1302210006;
+using System.Collections.Generic;
+using System.Numerics;
+
+BankTransferConfig_1302210006 bTfCon = new BankTransferConfig_1302210006();
+int biaya;
+
+if (bTfCon.bankTf.lang == "en")
+{
+    Console.WriteLine("Please insert the amount of money to transfer: ");
+    int jumlah = Convert.ToInt32(Console.ReadLine());
+    if (jumlah <= bTfCon.bankTf.transfer.threshold)
+    {
+        biaya = bTfCon.bankTf.transfer.low_fee;
+    }
+    else {
+        biaya = bTfCon.bankTf.transfer.high_fee;
+    }
+
+    int ress = jumlah + biaya;
+
+    Console.WriteLine("Transfer fee = " + biaya);
+    Console.WriteLine("Total amount = " + ress);
+
+    Console.WriteLine("Select transfer method: ");
+
+    for (int i = 0; i < bTfCon.bankTf.methods.Count; i++)
+    {
+        Console.WriteLine((i + 1) + ". " + bTfCon.bankTf.methods[i]);
+    }
+
+    Console.WriteLine("Please type " + bTfCon.bankTf.confirmation.en + "to confirm the transaction: ");
+    String confirm = Console.ReadLine();
+
+    if (confirm == bTfCon.bankTf.confirmation.en)
+    {
+        Console.WriteLine("The transfer is completed");
+    }
+    else {
+        Console.WriteLine("Transfer is cancelled");
+    }
+}
+else if (bTfCon.bankTf.lang == "id") {
+    Console.WriteLine("Masukkan jumlah uang yang akan di-transfer: ");
+    int jumlah = Convert.ToInt32(Console.ReadLine());
+    if (jumlah <= bTfCon.bankTf.transfer.threshold)
+    {
+        biaya = bTfCon.bankTf.transfer.low_fee;
+    }
+    else
+    {
+        biaya = bTfCon.bankTf.transfer.high_fee;
+    }
+
+    int ress = jumlah + biaya;
+
+    Console.WriteLine("Biaya transfer = " + biaya);
+    Console.WriteLine("Total biaya = " + ress);
+    Console.WriteLine("Pilih metode transfer: ");
+
+    for (int i = 0; i < bTfCon.bankTf.methods.Count; i++) { 
+        Console.WriteLine((i + 1) + ". " + bTfCon.bankTf.methods[i]);
+    }
+
+    Console.WriteLine("Ketik " + bTfCon.bankTf.confirmation.id + "untuk mengkonfirmasi transaksi: ");
+    String confirm = Console.ReadLine();
+
+    if (confirm == bTfCon.bankTf.confirmation.id)
+    {
+        Console.WriteLine("Proses transfer berhasil");
+    }
+    else
+    {
+        Console.WriteLine("Transfer dibatalkan");
+    }
+}
